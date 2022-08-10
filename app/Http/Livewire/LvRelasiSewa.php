b4f0cabab8ce110216f59d5858f317b8994ca333
Muhammad Ihsan Hafizhin <80156673+ihsanhafizhin@users.noreply.github.com>
2022-08-10 13:37:20 +0700

    Dev academy (#2)
    * push soal nomor 1
    
    * revisi push soal nomor 1
    
    * push soal nomor 2
    
    * push soal nomor 3
    
    * push soal nomor 3
    
    * push soal nomor 4
    
    * push soal nomor 5
    
    * push soal nomor 7
    
    * push soal nomor 8:
    
    * push soal nomor 7
    
    Co-authored-by: Muhammad Ihsan Hafizhin <ztehafiz@gmail.com>

--- app/Http/Livewire/LvRelasiSewa.php
+++ app/Http/Livewire/LvRelasiSewa.php
@@ -3,7 +3,7 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use App\Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 use App\Models\{
     Customer,
     Sewa,
@@ -13,6 +13,23 @@ use App\Models\{
 class LvRelasiSewa extends Component
 {
     public $relation_ones = [];
+
+    public $relation_twos = [];
+
+    public $relation_threes = [];
+
+    public $relation_fours = [];
+
+    public $relation_fives = [];
+
+    public $relation_sixs = [];
+
+    public $relation_sevens = [];
+
+    public $relation_eights = [];
+
+    public $relation_nines = [];
+
 
     public function render()
     {
@@ -34,5 +51,98 @@ class LvRelasiSewa extends Component
         ->get();
     }
 
+    public function setRelationTwos()
+    {
+        $this->relation_twos = Sewa::query()
+        ->select('sewas.*', 'fl.Judul AS Judul_Film')
+        ->join('customers AS cs', 'cs.No_Identitas', 'sewas.No_Identitas')
+        ->join('films AS fl', 'fl.Kd_Film', 'sewas.Kd_Film')
+        ->where('tgl_pinjam', '=', '2019-03-20')
+        ->orWhere('tgl_pinjam', '=', '2019-03-21')
+        ->get();
+    }
 
+    public function setRelationThrees()
+    {
+        $this->relation_threes = Sewa::query()
+        ->select('sewas.*', 'cs.Nama AS Nama_Customer','cs.Alamat')
+        ->join('customers AS cs', 'cs.No_Identitas', 'sewas.No_Identitas')
+        ->join('films AS fl', 'fl.Kd_Film', 'sewas.Kd_Film')
+        ->where('tgl_pinjam', '=', '2019-03-24')
+        ->orWhere('tgl_pinjam', '=', '2019-03-25')
+        ->get();
+    }
+
+    public function setRelationFours()
+    {
+        $this->relation_fours = Sewa::query()
+        ->select('sewas.*', 'fl.Jenis','fl.Judul', 'Harga_Sewa')
+        ->join('customers AS cs', 'cs.No_Identitas', 'sewas.No_Identitas')
+        ->join('films AS fl', 'fl.Kd_Film', 'sewas.Kd_Film')
+        ->whereBetween('Harga_Sewa', [25000, 100000])
+        ->get();
+    }
+
+    public function setRelationFives()
+    {
+        $this->relation_fives = Sewa::query()
+        ->select('sewas.*', 'cs.Nama AS Nama_Customer','fl.Judul', 'Harga_Sewa', 'cs.Alamat')
+        ->join('customers AS cs', 'cs.No_Identitas', 'sewas.No_Identitas')
+        ->join('films AS fl', 'fl.Kd_Film', 'sewas.Kd_Film')
+        ->where('Alamat', 'like', '%Depok')
+        ->get();
+    }
+
+    public function setRelationSixs()
+    {
+        $this->relation_sixs =  Sewa::query()
+        ->select('sewas.*', 'cs.Nama AS Nama_Customer','cs.Alamat', 'fl.Judul', 'cs.Jenis_Identitas')
+        ->join('customers AS cs', 'cs.No_Identitas', 'sewas.No_Identitas')
+        ->join('films AS fl', 'fl.Kd_Film', 'sewas.Kd_Film')
+        ->where('Jenis_Identitas', '=', 'SIM')
+        ->get();
+    }
+
+    
+    public function setRelationSevens()
+    {
+        $this->relation_sevens = Sewa::query()
+        ->select('sewas.*', 'cs.Nama AS Nama_Customer' )
+        ->join('customers AS cs', 'cs.No_Identitas', 'sewas.No_Identitas')
+        ->join('films AS fl', 'fl.Kd_Film', 'sewas.Kd_Film')
+        ->groupBy('sewas.Kd_Sewa')
+        ->havingRaw('count(*) = 2')
+        ->get();
+
+        $this->itung_film = Sewa::query()
+        ->select('sewas.*', 'cs.Nama AS Nama_Customer' )
+        ->join('customers AS cs', 'cs.No_Identitas', 'sewas.No_Identitas')
+        ->join('films AS fl', 'fl.Kd_Film', 'sewas.Kd_Film')
+        ->groupBy('sewas.Kd_Sewa')
+        ->havingRaw('count(*) > 1')
+        ->count();
+    }
+
+    public function setRelationEights()
+    {
+        $this->relation_eights = Sewa::query()
+        ->select('sewas.*', 'cs.Nama AS Nama_Customer','cs.Alamat', 'fl.Judul', 'cs.Jenis_Identitas')
+        ->join('customers AS cs', 'cs.No_Identitas', 'sewas.No_Identitas')
+        ->join('films AS fl', 'fl.Kd_Film', 'sewas.Kd_Film')
+        ->where('Jenis_Identitas', '=', 'KTP')
+        ->get();
+    }
+
+    public function setRelationNines()
+    {
+        $from = date('2019-03-25');
+        $to = date('2019-03-28');
+
+        $this->relation_nines = Sewa::query()
+        ->select('sewas.*', 'cs.No_Identitas', 'cs.Jenis_Identitas', 'cs.Nama AS Nama_Customer', 'cs.Alamat')
+        ->join('customers AS cs', 'cs.No_Identitas', 'sewas.No_Identitas')
+        ->join('films AS fl', 'fl.Kd_Film', 'sewas.Kd_Film')
+        ->whereBetween('Tgl_Kembali', [$from, $to])
+        ->get();
+    }
 }
--- resources/views/livewire/tasks/lv_task_1.blade.php
+++ resources/views/livewire/tasks/lv_task_1.blade.php
@@ -133,6 +133,316 @@
                 </div>
             </div>
         </div>
+        <div class="col-12">
+            <hr>
+        </div>
+        <div class="col-md-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-white">
+                    <h4 class="card-title">Relations 2</h4>
+                    <p>Informasi judul film yang disewa pada tanggal 20 dan 21 Maret 2019</p>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nama Film</th>
+                                <th>Tanggal Pinjam</th>
+                                <th>Tanggal Kembali</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <x-tables.loading-state :column-size='6' :target="'setRelationTwos'" />
+                            @forelse ($relation_twos as $rl_two)
+                            <tr wire:key="{{'row-rls-2-'.$rl_two->Kd_Sewa}}">
+                                <td> {{$rl_two->Judul_Film}} </td>
+                                <td> {{$rl_two->Tgl_Pinjam}} </td>
+                                <td> {{$rl_two->Tgl_Kembali}} </td>
+                            </tr>
+                            @empty
+                            <x-tables.empty-data :colum-size='6' :target="'setRelationTwos'" />
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-end">
+                    <button class="btn btn-primary" wire:click="setRelationTwos">Show Data</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <hr>
+        </div>
+        <div class="col-md-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-white">
+                    <h4 class="card-title">Relations 3</h4>
+                    <p>Informasi nama dan alamat customer yang menyewa pada tanggal 24 dan 25 
+                        Maret 2019
+                    </p>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nama Customer</th>
+                                <th>Alamat Customer</th>
+                                <th>Tanggal Pinjam</th>
+                                <th>Tanggal Kembali</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <x-tables.loading-state :column-size='6' :target="'setRelationThrees'" />
+                            @forelse ($relation_threes as $rl_three)
+                            <tr wire:key="{{'row-rls-3-'.$rl_three->Kd_Sewa}}">
+                                <td> {{$rl_three->Nama_Customer}} </td>
+                                <td> {{$rl_three->Alamat}} </td>
+                                <td> {{$rl_three->Tgl_Pinjam}} </td>
+                                <td> {{$rl_three->Tgl_Kembali}} </td>
+                            </tr>
+                            @empty
+                            <x-tables.empty-data :colum-size='6' :target="'setRelationThrees'" />
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-end">
+                    <button class="btn btn-primary" wire:click="setRelationThrees">Show Data</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <hr>
+        </div>
+        <div class="col-md-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-white">
+                    <h4 class="card-title">Relations 4</h4>
+                    <p>Informasi judul film dan jenis film yang harga sewanya paling tinggi!</p>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nama Film</th>
+                                <th>Jenis Film</th>
+                                <th>Harga Sewa</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <x-tables.loading-state :column-size='6' :target="'setRelationFours'" />
+                            @forelse ($relation_fours as $rl_fours)
+                            <tr wire:key="{{'row-rls-3-'.$rl_fours->Kd_Sewa}}">
+                                <td> {{$rl_fours->Judul}} </td>
+                                <td> {{$rl_fours->Jenis}} </td>
+                                <td> {{$rl_fours->Harga_Sewa}} </td>
+                            </tr>
+                            @empty
+                            <x-tables.empty-data :colum-size='6' :target="'setRelationFours'" />
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-end">
+                    <button class="btn btn-primary" wire:click="setRelationFours">Show Data</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <hr>
+        </div>
+        <div class="col-md-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-white">
+                    <h4 class="card-title">Relations 5</h4>
+                    <p>Informasi nama customer, judul film dan harga sewa untuk customer yang tinggal di kota Depok</p>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nama Customer</th>
+                                <th>Nama Film</th>
+                                <th>Harga Sewa</th>
+                                <th>Alamat</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <x-tables.loading-state :column-size='6' :target="'setRelationFives'" />
+                            @forelse ($relation_fives as $rl_fives)
+                            <tr wire:key="{{'row-rls-3-'.$rl_fives->Kd_Sewa}}">
+                                <td> {{$rl_fives->Nama_Customer}} </td>
+                                <td> {{$rl_fives->Judul}} </td>
+                                <td> {{$rl_fives->Harga_Sewa}} </td>
+                                <td> {{$rl_fives->Alamat}} </td>
+                            </tr>
+                            @empty
+                            <x-tables.empty-data :colum-size='6' :target="'setRelationFives'" />
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-end">
+                    <button class="btn btn-primary" wire:click="setRelationFives">Show Data</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <hr>
+        </div>
+        <div class="col-md-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-white">
+                    <h4 class="card-title">Relations 6</h4>
+                    <p>Informasi dari nama, alamat customer dan judul film saat menyewa menggunakan identitas berupa SIM</p>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nama Customer</th>
+                                <th>Alamat</th>
+                                <th>Nama Film</th>
+                                <th>Jenis Identitas</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <x-tables.loading-state :column-size='6' :target="'setRelationSixs'" />
+                            @forelse ($relation_sixs as $rl_sixs)
+                            <tr wire:key="{{'row-rls-3-'.$rl_sixs->Kd_Sewa}}">
+                                <td> {{$rl_sixs->Nama_Customer}} </td>
+                                <td> {{$rl_sixs->Alamat}} </td>
+                                <td> {{$rl_sixs->Judul}} </td>
+                                <td> {{$rl_sixs->Jenis_Identitas}} </td>
+                            </tr>
+                            @empty
+                            <x-tables.empty-data :colum-size='6' :target="'setRelationSixs'" />
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-end">
+                    <button class="btn btn-primary" wire:click="setRelationSixs">Show Data</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <hr>
+        </div>
+        <div class="col-md-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-white">
+                    <h4 class="card-title">Relations 7</h4>
+                    <p>Informasi dari nama customer, kode sewa dan jumlah sewa dari masing - masing kode sewa untuk customer yang menyewa 2 film</p>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nama Customer</th>
+                                <th>Kode Sewa</th>
+                                <th>Jumlah Sewa</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <x-tables.loading-state :column-size='6' :target="'setRelationSevens'" />
+                            @forelse ($relation_sevens as $rl_sevens)
+                            <tr wire:key="{{'row-rls-3-'.$rl_sevens->Kd_Sewa}}">
+                                <td> {{$rl_sevens->Nama_Customer}} </td>
+                                <td> {{$rl_sevens->Kd_Sewa}} </td>
+                                <td> {{$itung_film}} </td>
+                            </tr>
+                            @empty
+                            <x-tables.empty-data :colum-size='6' :target="'setRelationSevens'" />
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-end">
+                    <button class="btn btn-primary" wire:click="setRelationSevens">Show Data</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <hr>
+        </div>
+        <div class="col-md-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-white">
+                    <h4 class="card-title">Relations 8</h4>
+                    <p>Informasi dari nama, alamat customer dan judul film saat menyewa menggunakan identitas berupa KTP</p>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Nama Customer</th>
+                                <th>Alamat</th>
+                                <th>Nama Film</th>
+                                <th>Jenis Identitas</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <x-tables.loading-state :column-size='6' :target="'setRelationEights'" />
+                            @forelse ($relation_eights as $rl_eights)
+                            <tr wire:key="{{'row-rls-3-'.$rl_eights->Kd_Sewa}}">
+                                <td> {{$rl_eights->Nama_Customer}} </td>
+                                <td> {{$rl_eights->Alamat}} </td>
+                                <td> {{$rl_eights->Judul}} </td>
+                                <td> {{$rl_eights->Jenis_Identitas}} </td>
+                            </tr>
+                            @empty
+                            <x-tables.empty-data :colum-size='6' :target="'setRelationEights'" />
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-end">
+                    <button class="btn btn-primary" wire:click="setRelationEights">Show Data</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <hr>
+        </div>
+        <div class="col-md-12 mb-4">
+            <div class="card shadow-sm">
+                <div class="card-header bg-white">
+                    <h4 class="card-title">Relations 9</h4>
+                    <p>Seluruh informasi customer yang mengembalikan film antara tanggal 25 s.d. 28 Maret 2019</p>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>No Identitas</th>
+                                <th>Jenis Identitas</th>
+                                <th>Nama Customer</th>
+                                <th>Alamat</th>
+                                <th>Tanggal Kembali</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <x-tables.loading-state :column-size='6' :target="'setRelationNines'" />
+                            @forelse ($relation_nines as $rl_nines)
+                            <tr wire:key="{{'row-rls-3-'.$rl_nines->Kd_Sewa}}">
+                                <td> {{$rl_nines->No_Identitas}} </td>
+                                <td> {{$rl_nines->Jenis_Identitas}} </td>
+                                <td> {{$rl_nines->Nama_Customer}} </td>
+                                <td> {{$rl_nines->Alamat}} </td>
+                                <td> {{$rl_nines->Tgl_Kembali}} </td>
+                            </tr>
+                            @empty
+                            <x-tables.empty-data :colum-size='6' :target="'setRelationNines'" />
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer text-end">
+                    <button class="btn btn-primary" wire:click="setRelationNines">Show Data</button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
